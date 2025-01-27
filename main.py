76f23c8894a2505f73ae971933d5b6cc797d805c
Arturo Sandoval <artssc925@gmail.com>
2025-01-27 14:08:51 -0600

    Agregado de archivo .csv

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+import csv
+
 class Item:
     pay_rate = 10
+    every = []
     def __init__(self, name, price, quantity):
-        
-        assert price >= 0, f"Price {price} is less than zero."
-        assert quantity >= 4
         self.name = name
         self.price = price
         self.quantity = quantity
+        
+        Item.every.append(self) 
     
     def calculatePrice(self):
         return self.price * self.quantity
     
     def applyDiscount(self):
-        return self.price * self.pay_rate
+        return self.price * Item.pay_rate
+    
+    @classmethod #esto te permite crear y trabajar con instancias directamente en la clase sin necesidad de crear una clase exterior.
+    def instantiate_from_csv(cls): 
+        with open("items.csv","r") as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            Item(item.get("name"), item.get("price"), item.get("quantity"))
+    
+    @staticmethod
+    def is_integer(num):
+       if isinstance(num, float):
+           return num.is_integer()
+    
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 
-item1 = Item("Phone",91,30)
-
-
-item2 = Item("Laptop",150,13)
-
-print(item1.applyDiscount())
+print(Item.is_integer(10.0))
